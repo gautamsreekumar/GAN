b7b59553e1c882b33b40eaf0986470d1c5b4f55c
Gautam <gautamsree@gmail.com>
2017-12-22 10:15:34 +0530

    individual updation of variables
    variables are now updated individually

--- gan.py
+++ gan.py
@@ -22,7 +22,6 @@ g_b1 = tf.Variable(tf.zeros([g_hidden_layer, 1], dtype=tf.float64), name='g_b1')
 g_W2 = tf.Variable(tf.zeros([g_output_layer, g_hidden_layer], dtype=tf.float64), name='g_W2')
 g_b2 = tf.Variable(tf.zeros([g_output_layer, 1], dtype=tf.float64), name='g_b2')
 g_y = tf.nn.softmax(tf.matmul(g_W2, tf.matmul(g_W1, z)+g_b1)+g_b2)
-tf.cast(g_y, tf.float64)
 
 alpha_d = 0.2
 d_input_layer = m
@@ -37,39 +36,42 @@ d_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, x)+d_b1)+d_b2)
 d_g_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, g_y)+d_b1)+d_b2)
 
 unity = tf.Variable(1.0, dtype=tf.float64)
-maximize_this = tf.reduce_mean(tf.reduce_sum(tf.log(d_y)+tf.log(unity-d_g_y))) # the negative sign for
-# gradient ascent has already been included
-optim_d = [d_W1, d_b1, d_W2, d_b2]
+maximize_this = tf.reduce_mean(tf.reduce_sum(tf.log(d_y)+tf.log(unity-d_g_y)))
+optim_d = [d_W1, d_b1, d_W2, d_b2] # list of variables to be updated in d-network
 grad_d = tf.train.GradientDescentOptimizer(alpha_d).compute_gradients(maximize_this, var_list=optim_d)
-for item in range(len(optim_d)):
-    optim_d[item].assign(optim_d[item] + alpha_d*grad_d[item][0]) # gradient ascent
+'''for item in range(len(optim_d)):
+    optim_d[item].assign(optim_d[item] + alpha_d*grad_d[item][0]) # gradient ascent'''
+d_W1.assign(d_W1 + alpha_d*grad_d[0][0])
+d_b1.assign(d_b1 + alpha_d*grad_d[1][0])
+d_W2.assign(d_W2 + alpha_d*grad_d[2][0])
+d_b2.assign(d_b2 + alpha_d*grad_d[3][0])
 # train_step_d = tf.train.GradientDescentOptimizer(alpha_d).minimize(maximize_this)
 
 minimize_this = tf.reduce_mean(tf.reduce_sum(tf.log(unity-d_g_y)))
-optim_g = [g_W1, g_b1, g_W2, g_b2]
+optim_g = [g_W1, g_b1, g_W2, g_b2] # list of variables to updated in g-network
 grad_g = tf.train.GradientDescentOptimizer(alpha_g).compute_gradients(minimize_this, var_list=optim_g)
-for item in range(len(optim_g)):
-    optim_g[item].assign(optim_g[item] - alpha_g*grad_g[item][0]) # gradient descent
+'''for item in range(len(optim_g)):
+    optim_g[item].assign(optim_g[item] - alpha_g*grad_g[item][0]) # gradient descent'''
+g_W1.assign(g_W1 - alpha_g*grad_g[0][0])
+g_b1.assign(g_b1 - alpha_g*grad_g[1][0])
+g_W2.assign(g_W2 - alpha_g*grad_g[2][0])
+g_b2.assign(g_b2 - alpha_g*grad_g[3][0])
 # train_step_g = tf.train.GradientDescentOptimizer(alpha_g).minimize(minimize_this)
 
 sess = tf.InteractiveSession()
-tf.global_variables_initializer().run(session=sess)
+sess.run(tf.global_variables_initializer())
 
-print [v.name for v in tf.trainable_variables()]
 for iterations in range(number_of_iterations):
     for k in range(k_steps):
         sampled_x = nprand.normal(u_true, sigma_true, m).reshape(m, 1)
         sampled_z = nprand.normal(u_g, sigma_g, m).reshape(m, 1)
         # print sess.run(train_step_d, feed_dict={x: sampled_x, z:sampled_z}), "training discriminator"
-        sess.run(grad_d, feed_dict={x: sampled_x, z:sampled_z})
+        sess.run(optim_d, feed_dict={x: sampled_x, z:sampled_z})
         print "training discriminator"
-        
     sampled_z = nprand.normal(u_g, sigma_g, m).reshape(m, 1)
     # print sess.run(train_step_g, feed_dict={z: sampled_z}), "training generator"
-    sess.run(grad_g, feed_dict={z:sampled_z})
+    sess.run(optim_g, feed_dict={z:sampled_z})
     print "training generator"
-
-print sess.run(d_W2)
 
 for testing in range(test_cases):
     sampled_z = nprand.normal(u_g, sigma_g, m).reshape(m, 1)
