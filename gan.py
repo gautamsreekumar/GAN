4ab4696cccbb5262de9aef27faee9a7b79724fbd
Gautam <gautamsree@gmail.com>
2017-12-24 22:30:17 +0530

    using tf.variable_scope()

--- gan.py
+++ gan.py
@@ -12,28 +12,41 @@ k_steps = 10 # learning ratio of discriminator to that of the generator
 m = 100 # batch size
 test_cases = 1000
 
-alpha_g = 0.2
+alpha = 0.01
+alpha_g = alpha
 g_input_layer = m
 g_output_layer = m
 g_hidden_layer = 2*m
-z = tf.placeholder(tf.float64, shape=(g_input_layer, None))
-g_W1 = tf.Variable(tf.zeros([g_hidden_layer, g_input_layer], dtype=tf.float64), name='g_W1')
-g_b1 = tf.Variable(tf.zeros([g_hidden_layer, 1], dtype=tf.float64), name='g_b1')
-g_W2 = tf.Variable(tf.zeros([g_output_layer, g_hidden_layer], dtype=tf.float64), name='g_W2')
-g_b2 = tf.Variable(tf.zeros([g_output_layer, 1], dtype=tf.float64), name='g_b2')
-g_y = tf.nn.softmax(tf.matmul(g_W2, tf.matmul(g_W1, z)+g_b1)+g_b2)
+with tf.variable_scope("g_network", reuse=tf.AUTO_REUSE):
+    z = tf.placeholder(tf.float64, shape=(g_input_layer, None))
+    '''g_W1 = tf.get_variable(tf.zeros([g_hidden_layer, g_input_layer], dtype=tf.float64), name='g_W1')
+    g_b1 = tf.get_variable(tf.zeros([g_hidden_layer, 1], dtype=tf.float64), name='g_b1')
+    g_W2 = tf.get_variable(tf.zeros([g_output_layer, g_hidden_layer], dtype=tf.float64), name='g_W2')
+    g_b2 = tf.get_variable(tf.zeros([g_output_layer, 1], dtype=tf.float64), name='g_b2')'''
+    g_W1 = tf.get_variable('g_W1', [g_hidden_layer, g_input_layer], dtype=tf.float64)
+    g_b1 = tf.get_variable('g_b1', [g_hidden_layer, 1], dtype=tf.float64)
+    g_W2 = tf.get_variable('g_W2', [g_output_layer, g_hidden_layer], dtype=tf.float64)
+    g_b2 = tf.get_variable('g_b2', [g_output_layer, 1], dtype=tf.float64)
+    g_y = tf.nn.softmax(tf.matmul(g_W2, tf.matmul(g_W1, z)+g_b1)+g_b2)
 
-alpha_d = 0.2
+alpha_d = alpha
 d_input_layer = m
 d_output_layer = 1
 d_hidden_layer = 2*m
-x = tf.placeholder(tf.float64, shape=(d_input_layer, None))
-d_W1 = tf.Variable(tf.zeros([d_hidden_layer, d_input_layer], dtype=tf.float64), name='d_W1')
-d_b1 = tf.Variable(tf.zeros([d_hidden_layer, 1], dtype=tf.float64), name='d_b1')
-d_W2 = tf.Variable(tf.zeros([d_output_layer, d_hidden_layer], dtype=tf.float64), name='d_W2')
-d_b2 = tf.Variable(tf.zeros([d_output_layer, 1], dtype=tf.float64), name='d_b2')
-d_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, x)+d_b1)+d_b2)
-d_g_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, g_y)+d_b1)+d_b2)
+with tf.variable_scope("d_network", reuse=tf.AUTO_REUSE):
+    x = tf.placeholder(tf.float64, shape=(d_input_layer, None))
+    '''d_W1 = tf.get_variable(tf.zeros([d_hidden_layer, d_input_layer], dtype=tf.float64), name='d_W1')
+    d_b1 = tf.get_variable(tf.zeros([d_hidden_layer, 1], dtype=tf.float64), name='d_b1')
+    d_W2 = tf.get_variable(tf.zeros([d_output_layer, d_hidden_layer], dtype=tf.float64), name='d_W2')
+    d_b2 = tf.get_variable(tf.zeros([d_output_layer, 1], dtype=tf.float64), name='d_b2')
+    d_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, x)+d_b1)+d_b2)
+    d_g_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, g_y)+d_b1)+d_b2)'''
+    d_W1 = tf.get_variable('d_W1', [d_hidden_layer, d_input_layer], dtype=tf.float64)
+    d_b1 = tf.get_variable('d_b1', [d_hidden_layer, 1], dtype=tf.float64)
+    d_W2 = tf.get_variable('d_W2', [d_output_layer, d_hidden_layer], dtype=tf.float64)
+    d_b2 = tf.get_variable('d_b2', [d_output_layer, 1], dtype=tf.float64)
+    d_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, x)+d_b1)+d_b2)
+    d_g_y = tf.nn.softmax(tf.matmul(d_W2, tf.matmul(d_W1, g_y)+d_b1)+d_b2)
 
 unity = tf.Variable(1.0, dtype=tf.float64)
 maximize_this = tf.reduce_mean(tf.reduce_sum(tf.log(d_y)+tf.log(unity-d_g_y)))
@@ -45,7 +58,7 @@ for item in range(len(optim_d)):
 d_b1.assign(d_b1 + alpha_d*grad_d[1][0])
 d_W2.assign(d_W2 + alpha_d*grad_d[2][0])
 d_b2.assign(d_b2 + alpha_d*grad_d[3][0])'''
-train_step_d = tf.train.GradientDescentOptimizer(alpha_d).minimize(-maximize_this)
+train_step_d = tf.train.GradientDescentOptimizer(alpha_d).minimize(-maximize_this, var_list=optim_d)
 
 minimize_this = tf.reduce_mean(tf.reduce_sum(tf.log(unity-d_g_y)))
 optim_g = [g_W1, g_b1, g_W2, g_b2] # list of variables to updated in g-network
@@ -56,23 +69,27 @@ for item in range(len(optim_g)):
 g_b1.assign(g_b1 - alpha_g*grad_g[1][0])
 g_W2.assign(g_W2 - alpha_g*grad_g[2][0])
 g_b2.assign(g_b2 - alpha_g*grad_g[3][0])'''
-train_step_g = tf.train.GradientDescentOptimizer(alpha_g).minimize(minimize_this)
+train_step_g = tf.train.GradientDescentOptimizer(alpha_g).minimize(minimize_this, var_list=optim_g)
 
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
 for iterations in range(number_of_iterations):
     for k in range(k_steps):
-        sampled_x = nprand.normal(u_true, sigma_true, m).reshape(m, 1)
+        with tf.variable_scope("d_network", reuse=tf.AUTO_REUSE):
+            sampled_x = nprand.normal(u_true, sigma_true, m).reshape(m, 1)
+            sampled_z = nprand.normal(u_g, sigma_g, m).reshape(m, 1)
+            print sess.run(train_step_d, feed_dict={x: sampled_x, z:sampled_z}), "training discriminator"
+            # sess.run(optim_d, feed_dict={x: sampled_x, z:sampled_z})
+            # print "training discriminator"
+    with tf.variable_scope("g_network", reuse=tf.AUTO_REUSE):
         sampled_z = nprand.normal(u_g, sigma_g, m).reshape(m, 1)
-        # print sess.run(train_step_d, feed_dict={x: sampled_x, z:sampled_z}), "training discriminator"
-        sess.run(optim_d, feed_dict={x: sampled_x, z:sampled_z})
-        print "training discriminator"
-    sampled_z = nprand.normal(u_g, sigma_g, m).reshape(m, 1)
-    # print sess.run(train_step_g, feed_dict={z: sampled_z}), "training generator"
-    sess.run(optim_g, feed_dict={z:sampled_z})
-    print "training generator"
+        print sess.run(train_step_g, feed_dict={z: sampled_z}), "training generator"
+        # sess.run(optim_g, feed_dict={z:sampled_z})
+        # print "training generator"
 
 for testing in range(test_cases):
     sampled_z = nprand.normal(u_g, sigma_g, m).reshape(m, 1)
     print "testing", sess.run(d_g_y, feed_dict={z:sampled_z})
+
+print sess.run(g_W2)
